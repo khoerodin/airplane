2568b799f6433cefc8be51660e08bb82f2e9ce4a
Khoerodin <heykhoerodin@gmail.com>
2023-12-01 06:17:43 +0700

    Get Started Page

---
+++ lib/ui/pages/get_started_page.dart
@@ -0,0 +1,74 @@
+import 'package:airplane/shared/theme.dart';
+import 'package:flutter/material.dart';
+
+class GetStartedPage extends StatelessWidget {
+  const GetStartedPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  'assets/image_get_started.png',
+                ),
+              ),
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  'Fly like a Bird',
+                  style: whiteTextStyle.copyWith(
+                    fontSize: 32,
+                    fontWeight: semiBold,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 35),
+                  child: Text(
+                    'Explore new world with us and let your self get an amazing experiences',
+                    style: whiteTextStyle.copyWith(
+                      fontSize: 16,
+                      fontWeight: light,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Container(
+                  width: 220,
+                  height: 55,
+                  margin: const EdgeInsets.only(
+                    top: 50,
+                    bottom: 80,
+                  ),
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: kPrimaryColor,
+                    ),
+                    onPressed: () {},
+                    child: Text(
+                      'Get Started',
+                      style: whiteTextStyle.copyWith(
+                        fontSize: 18,
+                        fontWeight: medium,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
