d7ae7b0da0f61c50c76879d4cb7298c90b52e64b
Khoerodin <heykhoerodin@gmail.com>
2023-12-04 07:02:58 +0700

    Add Page Cubit to Home Menu

--- lib/ui/widgtes/custom_bottom_navigation_item.dart
+++ lib/ui/widgtes/custom_bottom_navigation_item.dart
@@ -1,40 +1,48 @@
+import 'package:airplane/cubit/page_cubit.dart';
 import 'package:airplane/shared/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CustomBottomNavigationItem extends StatelessWidget {
+  final int index;
   final String imageURL;
-  final bool isSelected;
 
   const CustomBottomNavigationItem({
     super.key,
+    required this.index,
     required this.imageURL,
-    this.isSelected = false,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        const SizedBox(),
-        Container(
-          height: 24,
-          width: 24,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(imageURL),
+    return GestureDetector(
+      onTap: () {
+        context.read<PageCubit>().setPage(index);
+      },
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const SizedBox(),
+          Image.asset(
+            imageURL,
+            width: 24,
+            height: 24,
+            color: context.watch<PageCubit>().state == index
+                ? kPrimaryColor
+                : kGreyColor,
+          ),
+          Container(
+            width: 30,
+            height: 2,
+            decoration: BoxDecoration(
+              color: context.watch<PageCubit>().state == index
+                  ? kPrimaryColor
+                  : Colors.transparent,
+              borderRadius: BorderRadius.circular(18),
             ),
-          ),
-        ),
-        Container(
-          width: 30,
-          height: 2,
-          decoration: BoxDecoration(
-            color: isSelected ? kPrimaryColor : Colors.transparent,
-            borderRadius: BorderRadius.circular(18),
-          ),
-        )
-      ],
+          )
+        ],
+      ),
     );
   }
 }
